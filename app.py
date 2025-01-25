697ecaffe4047545823ca23ff7c16aee8699a24e
krithikanaidu <virtualshakti05@gmail.com>
2025-01-26 01:44:48 +0530

    first commit

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask, jsonify, request, render_template
+import os
+from flask import Flask, request, jsonify, render_template
 import joblib
 
 app = Flask(__name__)
 
-model = joblib.load('sa2_model.pkl')
+# Load your model
+model = joblib.load("sa2_model.pkl")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    review_text = data.get('review')
+    review_text = data.get("review")
 
     if not review_text:
-        return jsonify({'error': 'No review text detected'}),400
+        return jsonify({'error': 'Review Text not available'}), 400
 
     prediction = model.predict([review_text])[0]
-    sentiment = 'positive' if prediction == 2 else 'negative'
+    sentiment = 'Positive' if prediction == 2 else "Negative"
 
-    return jsonify({'review' : review_text, 'sentiment' : sentiment })
+    return jsonify({'review': review_text, 'sentiment': sentiment})
 
 if __name__ == '__main__':
-    app.run(debug = True)  
-    
-
- 
+    # Use the PORT environment variable or default to 8000
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
